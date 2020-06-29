68a16085d65389cb4c567794aa31807cca11f76b
Harjot Barn <harjotba@buffalo.edu>
2020-06-29 11:37:35 -0400

    cleaned BSLP urls and views

--- BSLP/urls.py
+++ BSLP/urls.py
@@ -1,9 +1,7 @@
-
 from django.urls import path, include
-from.views import homepage_BSLP
+from .views import homepage_BSLP
 
 urlpatterns = [
-     path('', homepage_BSLP, name="home-BSLP"),
-     path('get-seeded/', include('get_seeded.urls')),
-
+    path("", homepage_BSLP, name="home-BSLP"),
+    path("get-seeded/", include("get_seeded.urls")),
 ]
