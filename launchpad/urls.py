b88575d234ea3342362dcf195163a30b24f50b10
Harjot Barn <harjotba@buffalo.edu>
2021-08-13 13:04:37 -0400

    added dropbox

--- launchpad/urls.py
+++ launchpad/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls, name="ADMIN"),
+    path("admin/", admin.site.urls),
     path("", include("BSLP.urls")),
     path("auth/", include("launchpad_team_auth.urls")),
 ]
